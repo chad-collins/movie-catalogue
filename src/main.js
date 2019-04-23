7bfe71bb91adea4735bcdc24a152ba6ed2475f12
Chad <chadcollins.co@gmail.com>
2019-04-23 03:09:36 -0400

    added dynamic trailer to movies

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueCarousel from 'vue-carousel';
+import VueYouTubeEmbed from 'vue-youtube-embed'
+Vue.use(VueYouTubeEmbed)
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Showcase v-bind:movie="movie"/>
+    <Showcase v-bind:movie="movie" v-bind:videoId="videoId"/>
     <!-- <Carrousel categoryName="Trending Movies" v-bind:items="trending"/> -->
     <MovieRow categoryName="Now In Theaters" v-bind:movies="inTheaters"/>
     <MovieRow categoryName="Best of 90's Action" v-bind:movies="ninetiesAction"/>
@@ -24,7 +24,8 @@ export default {
       trending: [],
       inTheaters: [],
       ninetiesAction: [],
-      movie: {}
+      movie: {},
+      videoId:{}
     };
   },
   created() {
@@ -39,7 +40,7 @@ export default {
     axios
       .get(
         //In Theaters
-        "https://api.themoviedb.org/3/discover/movie?api_key=15d03a7e369cab34e74dc2b5087ab2e2&with_release_type=2|3&region=US"
+        "https://api.themoviedb.org/3/discover/movie?api_key=15d03a7e369cab34e74dc2b5087ab2e2&with_release_type=2|3&region=US&append_to_response=videos"
       )
       .then(res => (this.inTheaters = res.data.results))
       .then(
@@ -48,16 +49,24 @@ export default {
             Math.floor(Math.random() * this.inTheaters.length)
           ])
       )
-
+      .then(() =>(axios
+      .get(
+        "https://api.themoviedb.org/3/movie/" +
+          this.movie.id +
+          "/videos?api_key=15d03a7e369cab34e74dc2b5087ab2e2"
+      )
+      .then(res => (this.videoId = res.data.results[0]))))
       .catch(err => console.log(err));
 
     axios
       .get(
         //In Theaters
-        "https://api.themoviedb.org/3/discover/movie?api_key=15d03a7e369cab34e74dc2b5087ab2e2&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=1990-01-01&primary_release_date.lte=1999-12-31&vote_average.gte=6&with_genres=28"
+        "https://api.themoviedb.org/3/discover/movie?api_key=15d03a7e369cab34e74dc2b5087ab2e2&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=1990-01-01&primary_release_date.lte=1999-12-31&vote_average.gte=6&with_genres=28&append_to_response=videos"
       )
       .then(res => (this.ninetiesAction = res.data.results))
       .catch(err => console.log(err));
+
+    
   }
 };
 </script>
